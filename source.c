f24be34b4b29099ae530986ea2146ae5f838b0ef
Nate Langolf <nlangolf@gmail.com>
2016-01-09 20:42:00 -0600

    handle resize

--- source.c
+++ source.c
@@ -1,14 +1,44 @@
 #include <GLUT/glut.h>
 
-void Render(void)
+void Render()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glBegin(GL_TRIANGLES);
-   glVertex3f(-0.5,-0.5,0.0);
-   glVertex3f(0.5,0.0,0.0);
-   glVertex3f(0.0,0.5,0.0);
+   glVertex3f(-2,-2,-5.0);
+   glVertex3f(2,0.0,-5.0);
+   glVertex3f(0.0,2,-5.0);
   glEnd();
   glutSwapBuffers();
+}
+
+float CalculateRatio(int width, int height)
+{
+  if (height == 0)
+  {
+    return width;
+  }
+
+  return ((float)width)/height;
+}
+
+void ReshapeCallback(int width, int height)
+{
+  float ratio = CalculateRatio(width, height);
+
+  // Use the Projection Matrix
+  glMatrixMode(GL_PROJECTION);
+
+  // Reset Matrix
+  glLoadIdentity();
+
+  // Set the viewport to be the entire window
+  glViewport(0, 0, width, height);
+
+  // Set the correct perspective.
+  gluPerspective(45, ratio, 1, 100);
+
+  // Get Back to the Modelview
+  glMatrixMode(GL_MODELVIEW);
 }
 
 void DisplayCallback()
@@ -18,17 +48,18 @@ void DisplayCallback()
 
 int main(int argc, char **argv)
 {
-  // init GLUT and create Window
+  // init GLUT and create window
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowPosition(100,100);
-  glutInitWindowSize(320,320);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(320, 320);
   glutCreateWindow("Lighthouse3D - GLUT Tutorial");
 
   // register callbacks
   glutDisplayFunc(DisplayCallback);
+  glutReshapeFunc(ReshapeCallback);
 
-  // enter GLUT event processing cycle
+  // enter GLUT event processing loop
   glutMainLoop();
 
   return 1;
